bf007041367b6cf21052634fa29029df8c3938b4
Hoài Nam <namhuynhkhachoai@gmail.com>
2024-07-08 23:03:13 +0700

    [Nam] Màn hình quản lý loại đóng gói

---
+++ app/Models/Preparation_type.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class preparation_type extends Model
+{
+    use HasFactory;
+
+    protected $table = 'preparation_types';
+
+    protected $fillable = [
+        'name',
+    ];
+    public $timestamps = false;
+}
